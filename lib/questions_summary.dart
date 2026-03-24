1b00bdd9a770a946781165d0eb4fd7f7afed470c
satoshi-sh <satoss1108@gmail.com>
2026-03-24 18:09:04 -0500

    Finished combining columns and rows

--- lib/questions_summary.dart
+++ lib/questions_summary.dart
@@ -12,6 +12,19 @@ class QuestionsSummary extends StatelessWidget {
         return Row(
           children: [
             Text(((data['question_index'] as int) + 1).toString()),
+            Column(
+              children: [
+                Text(data['question'] as String),
+                SizedBox(
+                  height: 5,
+                ),
+                Text(data['user_answer'] as String),
+                SizedBox(
+                  height: 5,
+                ),
+                Text(data['correct_answer'] as String),
+              ],
+            ),
           ],
         );
       }).toList(),
